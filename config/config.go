7ee67b8245d66a8fbefdcfba62027c7f38038725
0xhelloweb3 <1245816264@qq.com>
2023-07-06 14:18:36 +0800

    feat: update mainnet factory address

--- config/config.go
+++ config/config.go
@@ -7,7 +7,7 @@ type Network struct {
 
 var BEVMMainnet = Network{
 	RpcUrl:                 "https://mainnet.chainx.org/rpc",
-	FactoryContractAddress: "0xeB789d5f6f66104AE9876175A5B9A03bDa0545A8",
+	FactoryContractAddress: "0x124e3E8D56db6ADA37aF2b7662F275D49BA850e6",
 }
 
 var BEVMTestnet = Network{
